dba84330ada524bafde651d3bd83654b90eeccb2
Anton Byrko <antonbyrko@gmail.com>
2020-07-30 20:21:11 +0300

    tAdditional

---
+++ CollectionAdditional/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionAdditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var b = GetCollection(a);
+
+            foreach (var i in a)
+            {
+                Console.Write(i +" ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var i in b)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static  IEnumerable GetCollection(int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                if (i % 2 == 0)
+                    yield return i;
+            }
+        }
+    } 
+    
+}
